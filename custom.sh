a59afe51a3d4083e95c214e7f7f55cc397155b5a
Real Roboto <62517727+realroboto@users.noreply.github.com>
2026-03-16 11:58:31 -0400

    Enable UDP GRO forwarding for network interfaces

--- custom.sh
+++ custom.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+ethtool -K eth1 rx-udp-gro-forwarding on rx-gro-list off
+ethtool -K pppoe-wan rx-udp-gro-forwarding on rx-gro-list off
+
 /etc/init.d/sysntpd enable
 /etc/init.d/sysntpd restart
 date
