4899126f2fcdec45fa4ad3ee64911b4cf2803333
Real Roboto <62517727+realroboto@users.noreply.github.com>
2026-04-13 21:39:28 -0400

    Comment out ethtool commands and add reboot
    Comment out ethtool commands and add reboot.

--- custom.sh
+++ custom.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-ethtool -K eth1 gro off
-ethtool -K pppoe-wan gro off
+#ethtool -K eth1 gro off
+#ethtool -K pppoe-wan gro off
 
 /etc/init.d/sysntpd enable
 /etc/init.d/sysntpd restart
 date
+
+reboot
 
 exit 0
